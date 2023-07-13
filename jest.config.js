bff958bcc392be6027f13b482d80bee28756826c
jlee0505 <hj960322@gmail.com>
2023-07-13 23:31:16 +0900

    init 추가 설정 - jest 테스트 설치 및 설정 - 테스트용 컴포넌트 삭제 - 폴더 구조 정리

---
+++ jest.config.js
@@ -0,0 +1,36 @@
+export const collectCoverageFrom = [
+  '**/*.{js,jsx,ts,tsx}',
+  '!**/*.d.ts',
+  '!**/node_modules/**',
+];
+export const moduleNameMapper = {
+  // Handle CSS imports (with CSS modules)
+  // https://jestjs.io/docs/webpack#mocking-css-modules
+  '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+
+  // Handle CSS imports (without CSS modules)
+  '^.+\\.(css|sass|scss)$': '<rootDir>/styles/__mocks__/styleMock.js',
+
+  // Handle image imports
+  // https://jestjs.io/docs/webpack#handling-static-assets
+  '^.+\\.(jpg|jpeg|png|gif|webp|avif|svg)$': `<rootDir>/__mocks__/fileMock.js`,
+
+  // Handle module aliases
+  '^@pages/(.*)$': '<rootDir>/pages/$1',
+  '^@components/(.*)$': '<rootDir>/components/$1',
+};
+export const setupFilesAfterEnv = ['<rootDir>/jest.setup.js'];
+export const testPathIgnorePatterns = [
+  '<rootDir>/node_modules/',
+  '<rootDir>/.next/',
+];
+export const testEnvironment = 'jsdom';
+export const transform = {
+  // Use babel-jest to transpile tests with the next/babel preset
+  // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
+  '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+};
+export const transformIgnorePatterns = [
+  '/node_modules/',
+  '^.+\\.module\\.(css|sass|scss)$',
+];
