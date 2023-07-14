09a97a733aef6ee214d0e6cba63f879eaee611a2
from1to2 <ngcd12345@gmail.com>
2023-07-14 13:42:01 +0900

    Header 적용

--- front-end/src/pages/MainPage.tsx
+++ front-end/src/pages/MainPage.tsx
@@ -1,8 +1,10 @@
 import PostContainer from "./PostContainer";
+import Header from "./Header";
 
 const MainPage:React.FC = () => {
   return (
     <div>
+      <Header/>
       <PostContainer/>
     </div>
   );
