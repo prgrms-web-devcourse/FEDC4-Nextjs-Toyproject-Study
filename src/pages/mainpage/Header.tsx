6611066cec4f51c48c05cca26a27b22ca397d7b6
hayamaster <dnfxmfkdbzos@naver.com>
2023-07-31 15:54:16 +0900

    refactor: Header파일에 기본 색상파일 참고하여 설정

--- src/pages/mainpage/Header.tsx
+++ src/pages/mainpage/Header.tsx
@@ -1,11 +1,8 @@
 import { LargeButton } from '../../components/common/Button';
-// import { colors } from '../designSystems/Colors';
 import { useState } from 'react';
 import { SmallButton } from '../../components/common/Button';
 
 const Header = () => {
-  const colors = { border: '#2D4053' };
-
   const isLogin = true;
 
   const [modalOpened, setModalOpened] = useState(false);
@@ -32,7 +29,7 @@ const Header = () => {
   return (
     <>
       <header
-        className={`px-[18%] h-20 bg-[#FBFBFD] border-b border-solid border-[${colors['border']}]`}
+        className={`px-[18%] h-20 bg-blue-gray-10 border-b border-solid border-blue-gray-880`}
       >
         <div className='flex flex-row h-20 justify-between pl-12 pr-10 py-5 text-center'>
           <div className='text-heading-1 leading-10 flex justify-center items-center select-none'>
@@ -49,7 +46,7 @@ const Header = () => {
                   >
                     <div
                       id='modal'
-                      className={`border border-current px-[20px] py-[24px] w-36 h-32 bg-white flex flex-col justify-around items-center gap-5 shadow-md absolute top-[5rem] -left-[4.5rem]`}
+                      className={`border border-current px-[20px] py-[24px] w-36 h-32 bg-blue-gray-10 flex flex-col justify-around items-center gap-5 shadow-md absolute top-[5rem] -left-[4.5rem]`}
                     >
                       <div className='block text-lg select-none'>어쩌구 님</div>
                       <SmallButton text={'로그아웃'} />
@@ -67,7 +64,7 @@ const Header = () => {
                           fillRule='evenodd'
                           clipRule='evenodd'
                           d='M21 41C32.0457 41 41 32.0457 41 21C41 9.9543 32.0457 1 21 1C9.9543 1 1 9.9543 1 21C1 32.0457 9.9543 41 21 41Z'
-                          stroke={colors['border']}
+                          stroke='#2D4053'
                         />
                       </svg>
                     </div>
@@ -86,7 +83,7 @@ const Header = () => {
                             d='M20 40C31.0457 40 40 31.0457 40 20C40 8.9543 31.0457 0 20 0C8.9543 0 0 8.9543 0 20C0 31.0457 8.9543 40 20 40Z'
                             fill='black'
                             fillOpacity='0.2'
-                            stroke={colors['border']}
+                            stroke='#2D4053'
                           />
                         </g>
                         <defs>
@@ -98,7 +95,7 @@ const Header = () => {
                     </div>
                     <div className='absolute top-[59%] left-1/2 -translate-x-1/2 -translate-y-1/2'>
                       <svg
-                        className={`fill-[#fff] stroke-1 stroke-${colors['border']}`}
+                        className={`fill-white stroke-1 stroke-[#2D4053]`}
                         xmlns='http://www.w3.org/2000/svg'
                         width='28'
                         height='29'
@@ -113,7 +110,7 @@ const Header = () => {
                         />
                         <path
                           d='M1 28C1 28 8 25 10 23C12 21 6 21 6 11C6 1 14 1 14 1C14 1 22 1 22 11C22 21 16 21 18 23C20 25 27 28 27 28'
-                          stroke={colors['border']}
+                          stroke='#2D4053'
                           strokeLinecap='round'
                         />
                       </svg>
