cf7f196c598167fc0f0bacbc5bf4fc3a0dcc3079
hayamaster <dnfxmfkdbzos@naver.com>
2023-07-31 19:38:25 +0900

    feat: post 무한스크롤 기능 구현

--- src/pages/mainpage/PostContainer.tsx
+++ src/pages/mainpage/PostContainer.tsx
@@ -1,16 +1,44 @@
 import Post from './Post';
 import { DUMMY_DATA } from './DummyData';
+import { useEffect, useRef, useState } from 'react';
 
 const PostContainer: React.FC = () => {
+  const totalCount = DUMMY_DATA.length;
+  const start = 0;
+  const limit = 12;
+  const endRef = useRef<HTMLDivElement>(null);
+  const [end, setEnd] = useState<number>(limit);
+
+  const showData = DUMMY_DATA.slice(start, end);
+
+  const handleInfiniteScroll = (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting && showData.length < totalCount) {
+        observer.unobserve(entry.target);
+        setEnd(end + limit > totalCount ? totalCount : end + limit);
+      }
+    });
+  };
+
+  useEffect(() => {
+    if (!endRef.current) return;
+
+    const observer = new IntersectionObserver(handleInfiniteScroll, {
+      threshold: 1.0,
+    });
+    endRef.current && observer.observe(endRef.current);
+  }, [handleInfiniteScroll]);
+
   return (
     <div className='flex flex-col pl-[18%] pr-[16%] justify-center items-center w-full bg-blue-gray-25'>
       <div className='container flex flex-wrap py-14'>
-        {DUMMY_DATA.map((post) => (
-          <div className='p-3 h-96' key={post.id}>
+        {showData.map((post, idx) => (
+          <div className='p-3 h-96' id={String(idx)} key={post.id}>
             <Post post={post} />
           </div>
         ))}
       </div>
+      <div id={String(end)} ref={endRef} />
     </div>
   );
 };
