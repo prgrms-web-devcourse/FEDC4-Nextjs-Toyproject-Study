b6e4aa966743745a98371561a977fdc2f783e982
eeseung <seunghee9941@gmail.com>
2023-08-06 05:31:38 +0900

    feat: 공통 헤더 구현

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,10 +1,8 @@
 import PostContainer from 'components/mainpage/PostContainer';
-import Header from 'components/mainpage/Header';
 
 const MainPage: React.FC = () => {
   return (
     <div>
-      <Header />
       <PostContainer />
     </div>
   );
