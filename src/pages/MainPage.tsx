d207c583ad465ee04f60f3779054afa0d692a628
from1to2 <ngcd12345@gmail.com>
2023-08-14 20:38:39 +0900

    feat: Modal ESC

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import PostContainer from 'components/mainpage/PostContainer';
 import Modal from 'pages/Modal/Modal';
 
@@ -13,6 +13,21 @@ const MainPage: React.FC = () => {
     setState({ postId: postId, showModal: !state.showModal });
     document.body.style.overflow = 'hidden';
   };
+
+  useEffect(() => {
+    const handleEscape = (e) => {
+      if (e.key === 'Escape') {
+        setState({ ...state, showModal: true });
+        document.body.style.overflow = 'unset';
+      }
+    };
+
+    document.addEventListener('keydown', handleEscape);
+
+    return () => {
+      document.removeEventListener('keydown', handleEscape);
+    };
+  }, []);
 
   return (
     <div>
