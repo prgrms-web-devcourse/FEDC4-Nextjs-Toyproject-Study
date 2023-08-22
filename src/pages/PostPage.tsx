fdc24d5dc6de7aaac937bfe67bc373b40bc05a32
from1to2 <ngcd12345@gmail.com>
2023-08-22 14:46:14 +0900

    feat: postinput, postinputguidbutton 반응형 2

--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -3,19 +3,28 @@ import PostInput from 'components/post/PostInput';
 import { useMediaQuery } from 'react-responsive';
 
 const PostPage = () => {
-  const isPcOrTablet = useMediaQuery({
-    query: '(min-width:1550px)',
+  const isPc = useMediaQuery({
+    query: '(min-width:1400px)',
+  });
+  const isTablet = useMediaQuery({
+    query: '(min-width:1195px) and (max-width:1399px)',
   });
   const isMobile = useMediaQuery({
-    query: '(max-width:1549px)',
+    query: '(max-width:1194px)',
   });
 
   return (
     <>
-      {isPcOrTablet && (
+      {isPc && (
         <div className={`flex flex-col`}>
           <PostGuidButton width='w-1/2' />
           <PostInput width='w-1/2' />
+        </div>
+      )}
+      {isTablet && (
+        <div className={`flex flex-col`}>
+          <PostGuidButton width='w-7/12' />
+          <PostInput width='w-7/12' />
         </div>
       )}
       {isMobile && (
