88720b0bee410837d1af2a38b0071904bd1ba23e
from1to2 <ngcd12345@gmail.com>
2023-08-23 17:33:14 +0900

    feat: 모바일 반응형 추가 구현, Large Button 추가

--- src/pages/Modal/Modal.tsx
+++ src/pages/Modal/Modal.tsx
@@ -4,6 +4,7 @@ import Comment from './Comment/Comments';
 import { getPostDetail } from 'api/postApi';
 import { postModalType, PostType } from 'interface/post';
 import { useMediaQuery } from 'react-responsive';
+import { LargeButton } from 'components/common/Button';
 
 interface ModalProps {
   modalOption: {
@@ -43,16 +44,19 @@ const Modal: FC<ModalProps> = ({ modalOption, closeModal }) => {
     return <div>Loading...</div>;
   }
 
-  const isPcOrTablet = useMediaQuery({
+  const isPc = useMediaQuery({
     query: '(min-width:1115px)',
   });
+  const isTablet = useMediaQuery({
+    query: '(min-width:720px) and (max-width:1114px)',
+  });
   const isMobile = useMediaQuery({
-    query: '(max-width:1114px)',
+    query: '(max-width:719px)',
   });
 
   return (
     <>
-      {isPcOrTablet && (
+      {isPc && (
         <div
           id={`background`}
           className={`fixed left-0 top-0 w-screen h-screen z-[250] bg-[rgba(0,0,0,0.5)] ${
@@ -107,7 +111,7 @@ const Modal: FC<ModalProps> = ({ modalOption, closeModal }) => {
           </div>
         </div>
       )}
-      {isMobile && (
+      {isTablet && (
         <div
           id={`background`}
           className={`fixed left-0 top-0 w-screen h-screen overflow-auto z-[250] bg-[rgba(0,0,0,0.5)] ${
@@ -124,6 +128,63 @@ const Modal: FC<ModalProps> = ({ modalOption, closeModal }) => {
               {state.postData && (
                 <>
                   <ImageView postData={state.postData} />
+                  <Comment
+                    likeCount={state.postData.likeCount}
+                    postId={state.postData.postId}
+                    isLike={state.postData.isLike}
+                    comment={state.postData.comment}
+                    addComment={(newComment) => {
+                      const updatedState = { ...state };
+                      if (updatedState.postData.comment) {
+                        updatedState.postData.comment = [
+                          ...updatedState.postData.comment,
+                          newComment,
+                        ];
+                      }
+                      setState(updatedState);
+                    }}
+                    clickLike={(isLike) => {
+                      const updatedState = { ...state };
+                      updatedState.postData.isLike = isLike;
+                      if (updatedState.postData.isLike) {
+                        updatedState.postData.likeCount =
+                          (updatedState.postData.likeCount || 0) + 1;
+                      } else {
+                        if (
+                          updatedState.postData.likeCount &&
+                          updatedState.postData.likeCount !== 0
+                        ) {
+                          updatedState.postData.likeCount -= 1;
+                        }
+                      }
+                      setState(updatedState);
+                    }}
+                  />
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
+      {isMobile && (
+        <div
+          id={`background`}
+          className={`fixed left-0 top-0 w-screen h-screen overflow-auto z-[250] bg-[rgba(0,0,0,0.5)] ${
+            showModal ? 'hidden' : ''
+          }`}
+          onClick={closeModal}
+        >
+          <div
+            className={`absolute left-1/2 top-1/2 w-full h-full text-center z-10 translate-y-[-50%] translate-x-[-50%]`}
+          >
+            <div
+              className={`flex flex-col bg-blue-gray-10 shadow-card-1 flex flex-row border border-solid border-blue-gray-800 justify-center items-center`}
+            >
+              <LargeButton text='닫기'></LargeButton>
+              {state.postData && (
+                <>
+                  <ImageView postData={state.postData} />
+                  this is mobile
                   <Comment
                     likeCount={state.postData.likeCount}
                     postId={state.postData.postId}
