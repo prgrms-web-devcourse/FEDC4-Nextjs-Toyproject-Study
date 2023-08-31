79081ff4c59b3b64aa80ab7df06ac08517ffb638
from1to2 <ngcd12345@gmail.com>
2023-08-31 11:54:35 +0900

    header 반성문작성아이콘, 햄버거바 - 기능추가구현,반응형디자인

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -5,7 +5,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { LargeButton, SmallButton } from './Button';
 import { AppDispatch, RootState } from '../../store';
 import { logout } from '../../store/auth';
-import user_profile from '../../assets/img/user_profile.svg';
+import hamburger from '../../assets/img/hamburger.svg';
+import pencil from '../../assets/img/pencil.svg';
 import { useMediaQuery } from 'react-responsive';
 
 const Header = () => {
@@ -26,6 +27,12 @@ const Header = () => {
   const handleLoginButtonClick = () => {
     navigate('/login');
   };
+  const handleMyPost = () => {
+    navigate('/my/post');
+  };
+  const handleMyInfo = () => {
+    navigate('/my/info');
+  };
 
   const handleLogout = () => {
     dispatch(logout());
@@ -33,16 +40,18 @@ const Header = () => {
     setIsModalOpen(false);
   };
 
-  const isPcOrTablet = useMediaQuery({
-    query: '(min-width:545px)',
+  const isPc = useMediaQuery({
+    query: '(min-width:891px)',
+  });
+  const isTablet = useMediaQuery({
+    query: '(min-width: 442px) and (max-width:890px)',
   });
   const isMobile = useMediaQuery({
-    query: '(max-width:544px)',
+    query: '(max-width:441px)',
   });
-
   return (
     <div>
-      {isPcOrTablet && (
+      {isPc && (
         <header className='border-b border-solid border-blue-gray-800 bg-blue-gray-10'>
           <div className='flex justify-between max-w-screen-xl mx-auto px-12 py-4'>
             <Link to='/'>
@@ -60,16 +69,35 @@ const Header = () => {
                   <div className='flex relative ml-8'>
                     <button onClick={handleModal}>
                       <img
-                        src={user_profile}
-                        alt='유저프로필'
+                        src={hamburger}
+                        alt='햄버거바'
                         width={42}
                         height={42}
                       />
                     </button>
                     {isModalOpen && (
-                      <div className='absolute top-20 left-1/2 -translate-x-1/2 flex flex-col justify-between items-center w-36 h-32 px-5 py-6 border bg-blue-gray-10 shadow-md z-10'>
-                        <div className='text-heading-4'>{auth.user.name}님</div>
-                        <SmallButton text={'로그아웃'} onClick={handleLogout} />
+                      <div className='absolute top-20 -right-16 -translate-x-1/2 flex flex-col justify-between items-center w-36 h-auto border bg-blue-gray-10 shadow-md z-10'>
+                        <div className='w-5/6 text-heading-4 border-b py-6 text-center font-semibold'>
+                          {auth.user.name} 님
+                        </div>
+                        <div
+                          className='cursor-pointer w-5/6 border-b py-2 text-center'
+                          onClick={handleMyPost}
+                        >
+                          내 반성문
+                        </div>
+                        <div
+                          className='cursor-pointer w-5/6 border-b py-2 text-center'
+                          onClick={handleMyInfo}
+                        >
+                          내 정보
+                        </div>
+                        <div
+                          className='cursor-pointer w-5/6 py-3 text-center'
+                          onClick={handleLogout}
+                        >
+                          로그아웃
+                        </div>
                       </div>
                     )}
                   </div>
@@ -86,52 +114,153 @@ const Header = () => {
           </div>
         </header>
       )}
-      {isMobile && (
-        <header className='text-center border-b border-solid border-blue-gray-800 bg-blue-gray-10'>
-          <Link to='/'>
-            <h1 className='p-4 text-heading-1'>고해성사</h1>
-          </Link>
-          <div className=''>
-            {auth.isLogin ? (
-              <div className='flex items-center mt-2 pb-6 justify-evenly'>
-                {location.pathname === '/' && (
-                  <LargeButton
-                    text={'반성문 쓰기'}
-                    onClick={handlePostButtonClick}
-                  />
-                )}
-                <button onClick={handleModal}>
-                  <div className=''>
-                    <div>
-                      <span className='inline-block w-5 h-11 align-middle'></span>
+      {isTablet && (
+        <header className='border-b border-solid border-blue-gray-800 bg-blue-gray-10'>
+          <div className='flex justify-between max-w-screen-xl mx-auto px-12 py-4'>
+            <Link to='/'>
+              <h1 className='p-4 text-heading-1'>고해성사</h1>
+            </Link>
+            <div className='flex items-center justify-evenly'>
+              {auth.isLogin ? (
+                <div className='flex items-center'>
+                  {location.pathname === '/' && (
+                    <button onClick={handlePostButtonClick}>
                       <img
-                        className='relative inline-block'
-                        src={user_profile}
-                        alt='유저프로필'
+                        src={pencil}
+                        alt='반성문작성'
                         width={42}
                         height={42}
                       />
-                      <span className='inline-block w-5 h-11 align-middle'></span>
+                    </button>
+                  )}
+                  <button onClick={handleModal}>
+                    <div className=''>
+                      <div>
+                        <span className='inline-block w-5 h-11 align-middle'></span>
+                        <img
+                          className='relative inline-block'
+                          src={hamburger}
+                          alt='햄버거바'
+                          width={42}
+                          height={42}
+                        />
+                        <span className='inline-block w-5 h-11 align-middle'></span>
+                      </div>
                     </div>
-                  </div>
-                </button>
-                {isModalOpen && (
-                  <div className='absolute top-40 left-3/4 -translate-x-1/2 flex flex-col justify-between items-center w-36 h-32 px-5 py-6 border bg-blue-gray-10 shadow-md z-10'>
-                    <div className='text-heading-4'>{auth.user.name}님</div>
-                    <SmallButton text={'로그아웃'} onClick={handleLogout} />
-                  </div>
-                )}
-              </div>
-            ) : (
-              location.pathname === '/' && (
-                <div className='pb-5'>
-                  <LargeButton
-                    text={'로그인'}
-                    onClick={handleLoginButtonClick}
-                  />
+                  </button>
+                  {isModalOpen && (
+                    <div className='absolute top-32 right-0 -translate-x-1/2 flex flex-col justify-between items-center w-36 h-auto border bg-blue-gray-10 shadow-md z-10'>
+                      <div className='w-5/6 text-heading-4 border-b py-6 text-center font-semibold'>
+                        {auth.user.name} 님
+                      </div>
+                      <div
+                        className='cursor-pointer w-5/6 border-b py-2 text-center'
+                        onClick={handleMyPost}
+                      >
+                        내 반성문
+                      </div>
+                      <div
+                        className='cursor-pointer w-5/6 border-b py-2 text-center'
+                        onClick={handleMyInfo}
+                      >
+                        내 정보
+                      </div>
+                      <div
+                        className='cursor-pointer w-5/6 py-3 text-center'
+                        onClick={handleLogout}
+                      >
+                        로그아웃
+                      </div>
+                    </div>
+                  )}
                 </div>
-              )
-            )}
+              ) : (
+                location.pathname === '/' && (
+                  <div className=''>
+                    <LargeButton
+                      style={{ width: '150px', height: '60px' }}
+                      text={'로그인'}
+                      onClick={handleLoginButtonClick}
+                    />
+                  </div>
+                )
+              )}
+            </div>
+          </div>
+        </header>
+      )}
+      {isMobile && (
+        <header className='border-b border-solid border-blue-gray-800 bg-blue-gray-10'>
+          <div className='flex justify-between max-w-screen-xl mx-auto px-2 py-2'>
+            <Link to='/'>
+              <h1 className='p-4 text-heading-1'>고해성사</h1>
+            </Link>
+            <div className='flex items-center justify-evenly'>
+              {auth.isLogin ? (
+                <div className='flex items-center'>
+                  {location.pathname === '/' && (
+                    <button onClick={handlePostButtonClick}>
+                      <img
+                        src={pencil}
+                        alt='반성문작성'
+                        width={42}
+                        height={42}
+                      />
+                    </button>
+                  )}
+                  <button onClick={handleModal}>
+                    <div className=''>
+                      <div>
+                        <span className='inline-block w-5 h-11 align-middle'></span>
+                        <img
+                          className='relative inline-block'
+                          src={hamburger}
+                          alt='햄버거바'
+                          width={42}
+                          height={42}
+                        />
+                        <span className='inline-block w-5 h-11 align-middle'></span>
+                      </div>
+                    </div>
+                  </button>
+                  {isModalOpen && (
+                    <div className='absolute top-28 -right-12 -translate-x-1/2 flex flex-col justify-between items-center w-36 h-auto border bg-blue-gray-10 shadow-md z-10'>
+                      <div className='w-5/6 text-heading-4 border-b py-6 text-center font-semibold'>
+                        {auth.user.name} 님
+                      </div>
+                      <div
+                        className='cursor-pointer w-5/6 border-b py-2 text-center'
+                        onClick={handleMyPost}
+                      >
+                        내 반성문
+                      </div>
+                      <div
+                        className='cursor-pointer w-5/6 border-b py-2 text-center'
+                        onClick={handleMyInfo}
+                      >
+                        내 정보
+                      </div>
+                      <div
+                        className='cursor-pointer w-5/6 py-3 text-center'
+                        onClick={handleLogout}
+                      >
+                        로그아웃
+                      </div>
+                    </div>
+                  )}
+                </div>
+              ) : (
+                location.pathname === '/' && (
+                  <div className=''>
+                    <LargeButton
+                      style={{ width: '150px', height: '60px' }}
+                      text={'로그인'}
+                      onClick={handleLoginButtonClick}
+                    />
+                  </div>
+                )
+              )}
+            </div>
           </div>
         </header>
       )}
